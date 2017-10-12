062738381a67b2ee9f036d4ab551eeb7e84344e6
Toky <tokymetal@gmail.com>
2017-10-12 23:33:26 +0300

    My
    oiehf

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter - And so it has been edited</title>
+	<title>Welcome to CodeIgniter - And so it has been edited </title>
 
 	<style type="text/css">
 
